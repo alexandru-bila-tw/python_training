1786d689ca1ce30c1ea41aef0420ec911d405876
alexandru-bila-tw <alexandru.bila@thoughtworks.com>
2026-07-02 14:55:21 +0300

    split hand in blackjack

--- Python_games/games/BlackJack.py
+++ Python_games/games/BlackJack.py
@@ -11,7 +11,7 @@ class BlackJack:
         print(f'Welcome to BlackJack {self.player.name}'.center(55))
         game_on = True
         while game_on:
-            player_cards = []
+            player_hands = [[]]
             cpu_cards = []
 
             if len(self.deck.all_cards) < 10:
@@ -19,63 +19,105 @@ class BlackJack:
                 self.deck.shuffle()
                 print('A new deck arrived'.center(55))
 
-            player_cards.append(self.deck.deal_one())
+            player_hands[0].append(self.deck.deal_one())
             cpu_cards.append(self.deck.deal_one())
-            player_cards.append(self.deck.deal_one())
+            player_hands[0].append(self.deck.deal_one())
             cpu_cards.append(self.deck.deal_one())
 
-            player_bust = False
-            player_forfeit = False
+            hand_statuses = []
 
-            while True:
-                player_score = self.calculate_score(player_cards)
+            hand_idx = 0
+            while hand_idx < len(player_hands):
+                current_hand = player_hands[hand_idx]
 
-                print('\n' + 'You can see the dealer has the card:'.center(55) + f'\n{cpu_cards[0]}\n' + 'You have:'.center(55))
-                for card in player_cards:
-                    print(str(card).center(55))
-                print(f'{self.player.name.upper()} has score {player_score}.'.center(55))
+                while True:
+                    player_score = self.calculate_score(current_hand)
 
-                if player_score > 21:
-                    print('💥 You BUSTED! 💥'.center(55))
-                    player_bust = True
-                    break
+                    if len(player_hands) > 1:
+                        print(f'\n--- PLAYING HAND {hand_idx + 1} OF {len(player_hands)} ---'.center(55))
 
-                choice = input('Do you (H)it, (S)tand, or (F)orfeit? '.center(55) + '\n').upper()
+                    print('\n' + 'You can see the dealer has the card:'.center(55) + f'\n{cpu_cards[0]}\n' + 'You have:'.center(55))
+                    for card in current_hand:
+                        print(str(card).center(55))
+                    print(f'{self.player.name.upper()} has score {player_score}.'.center(55))
 
-                if choice in ['H', 'HIT']:
-                    player_cards.append(self.deck.deal_one())
-                elif choice in ['S', 'STAND', 'PLAY']:
-                    break
-                elif choice in ['F', 'FORFEIT']:
-                    player_forfeit = True
-                    break
-                else:
-                    print("Invalid choice, try again.".center(55))
+                    if player_score > 21:
+                        print('💥 You BUSTED! 💥'.center(55))
+                        hand_statuses.append('bust')
+                        break
 
-            if not player_bust and not player_forfeit:
+                    can_split = len(current_hand) == 2 and current_hand[0].rank == current_hand[1].rank
+
+                    prompt = 'Do you (H)it, (S)tand, (F)orfeit'
+                    if can_split:
+                        prompt += ', or S(P)lit? '
+                    else:
+                        prompt += '? '
+
+                    choice = input(prompt.center(55) + '\n').upper()
+
+                    if choice in ['H', 'HIT']:
+                        current_hand.append(self.deck.deal_one())
+                    elif choice in ['S', 'STAND', 'PLAY']:
+                        hand_statuses.append('stand')
+                        break
+                    elif choice in ['F', 'FORFEIT']:
+                        hand_statuses.append('forfeit')
+                        break
+                    elif choice in ['P', 'SPLIT'] and can_split:
+                        print('--- Splitting Pair! ---'.center(55))
+                        split_card = current_hand.pop()
+                        new_hand = [split_card]
+
+                        current_hand.append(self.deck.deal_one())
+                        new_hand.append(self.deck.deal_one())
+
+                        player_hands.insert(hand_idx + 1, new_hand)
+
+                        continue
+                    else:
+                        print("Invalid choice, try again.".center(55))
+
+                hand_idx += 1
+
+            cpu_score = self.calculate_score(cpu_cards)
+
+            if 'stand' in hand_statuses:
                 print('\n' + "Dealer's Turn".center(55))
-                cpu_score = sum(card.value for card in cpu_cards)
+                cpu_score = self.calculate_score(cpu_cards)
 
                 while cpu_score < 17:
                     cpu_cards.append(self.deck.deal_one())
-                    cpu_score = sum(card.value for card in cpu_cards)
+                    cpu_score = self.calculate_score(cpu_cards)
 
                 print('Dealer has:'.center(55))
                 for card in cpu_cards:
                     print(str(card).center(55))
                 print(f'Dealer final score: {cpu_score}'.center(55))
+                print('\n' + ' FINAL RESULTS '.center(55, '='))
+            else:
+                print('\n' + ' FINAL RESULTS '.center(55, '='))
+                print("Dealer doesn't need to play.".center(55))
 
-                if cpu_score > 21:
-                    print('🎉 Dealer busted! You win! 🎉'.center(55))
-                elif player_score > cpu_score:
-                    print('🎉 You win! 🎉'.center(55))
-                elif player_score < cpu_score:
-                    print('👹 Dealer wins. 👹'.center(55))
+            for idx, hand in enumerate(player_hands):
+                status = hand_statuses[idx]
+                hand_score = self.calculate_score(hand)
+
+                hand_label = f"Hand {idx + 1}: " if len(player_hands) > 1 else ""
+
+                if status == 'bust':
+                    print(f'{hand_label}💥 Busted with {hand_score}. You lose! 💥'.center(55))
+                elif status == 'forfeit':
+                    print(f'{hand_label}Hand forfeited.'.center(55))
                 else:
-                    print('👔 It is a tie! 👔'.center(55))
-
-            elif player_forfeit:
-                print("Hand forfeited.".center(55))
+                    if cpu_score > 21:
+                        print(f'{hand_label}🎉 Dealer busted! You win with {hand_score}! 🎉'.center(55))
+                    elif hand_score > cpu_score:
+                        print(f'{hand_label}🎉 You win with {hand_score} vs {cpu_score}! 🎉'.center(55))
+                    elif hand_score < cpu_score:
+                        print(f'{hand_label}👹 Dealer wins with {cpu_score} vs {hand_score}. 👹'.center(55))
+                    else:
+                        print(f'{hand_label}👔 It is a tie at {hand_score}! 👔'.center(55))
 
             print('\n' + '-'*55)
             go_on = input("Do you want to play another hand? (Y/N): ".center(55) + '\n').upper()
